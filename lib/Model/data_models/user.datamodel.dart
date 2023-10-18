d711c85a0b3a7f9fb1848f4eee12a0ca55b9a63b
Otmane BACHRI <bachriotmane@gmail.com>
2023-10-18 12:18:32 +0100

    Add the firebase configuration to the project

---
+++ lib/Model/data_models/user.datamodel.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class UserDataModel {
+  String userId;
+  String userFirstNmae;
+  String userLastNmae;
+  String userProfileImage;
+  String userEmail;
+  String userPassword;
+  
+  UserDataModel({
+    required this.userId,
+    required this.userFirstNmae,
+    required this.userLastNmae,
+    required this.userProfileImage,
+    required this.userEmail,
+    required this.userPassword,
+  });
+
+}
